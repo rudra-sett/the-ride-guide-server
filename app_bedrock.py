ac92ab088cfed0b66843485fd265d95c6eb5c76a
Rudra Sett <rudro.sett@gmail.com>
2024-03-19 11:18:34 -0400

    first attempt at Claude support

--- app_bedrock.py
+++ app_bedrock.py
@@ -1,10 +1,17 @@
 from pydantic import BaseModel
 
 from langchain_community.llms import Bedrock
+from langchain_community.llms import BedrockChat
 from langchain.prompts import PromptTemplate
 
+from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage
+from langchain_core.messages import SystemMessage
+
+
+
 import boto3
-import json
+# import json
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -87,7 +94,7 @@ class ChatResponse(BaseModel):
     response: str
 
 # get the correct model and params
-def get_model(model_name = "meta.llama2-13b-chat-v1"):
+def get_model(model_name = "meta.llama2-13b-chat-v1",system=None,history=None):
     if model_name == "meta.llama2-13b-chat-v1":
         body = {
                 "max_gen_len": 1000,
@@ -110,6 +117,27 @@ def get_model(model_name = "meta.llama2-13b-chat-v1"):
                 "stop" : ["[INST]","[User]","[Assistant]","[\n]","[ ]","[\n ]","[ \n]","[Customer","Bot:","Human:"]
             }
         return Bedrock(client = bedrock, model_id="mistral.mistral-7b-instruct-v0:2",model_kwargs=body)
+    elif model_name == 'anthropic.claude-3-sonnet-20240229-v1:0' or model_name == 'anthropic.claude-v2:1':
+        body = {
+                "max_tokens": 1000,
+                # "system" : system,
+                # "messages" : history,
+                "temperature": 0.15,
+                "top_p": 0.96,
+                "stop_sequences" : ["[INST]","[User]","[Assistant]","[\n]","[ ]","[\n ]","[ \n]","[Customer","Bot:","Human:"]
+            }
+        return BedrockChat(client = bedrock, model_id=model_name,model_kwargs=body)
+
+
+def claude_message_api_formatter(system_message,message_history):
+    messages = []
+    messages.append(SystemMessage(content=system_message))
+    for msg in message_history:
+        # messages.append({"role" : "user","content" : msg["user"]})
+        # messages.append({"role" : "assistant","content" : msg["chatbot"]})
+        messages.append(HumanMessage(content=msg["user"]))
+        messages.append(AIMessage(content=msg["chatbot"]))
+    return messages
 
 # define the retriever and prompt
 retriever = index.vectorstore.as_retriever()
@@ -131,10 +159,6 @@ async def chat(request: Request):
     history = request_data["history"]
     model = request_data["model"]
 
-    # get the correct model
-    print(model)
-    llm = get_model(model)
-
     # get relevant documents
     docs = retriever.get_relevant_documents(message)
 
@@ -144,21 +168,38 @@ async def chat(request: Request):
     You are an AI chatbot for the RIDE, an MBTA paratransit service. You will help customer service representatives respond to user complaints and queries.
     Answer questions based on your knowledge and nothing more. If you are unable to decisively answer a question, direct them to customer service. Do not make up information outside of your given information.
     Customer service is needed if it is something you cannot answer. Requests for fare history require customer service, as do service complaints like a rude driver or late pickup.
-    Highly-specific situations will also require customer service to step in.'''
+    Highly-specific situations will also require customer service to step in.
+    Phone numbers:
+    TRAC (handles scheduling/booking, trip changes/cancellations, anything time-sensitive): 844-427-7433 (voice/relay) 857-206-6569 (TTY)
+    Mobility Center (handles eligibility questions, renewals, and changes to mobility status): 617-337-2727 (voice/relay)
+    MBTA Customer support (handles all other queries): 617-222-3200 (voice/relay)
+    '''
 
-    # assemble the chat history
-    history_str = ""
-    for msg in history:
-        history_str += f'[INST]\n{msg["user"]} [/INST]\n' 
-        history_str += f'{msg["chatbot"]}\n'
+    # get the correct model
+    llm = get_model(model)
+    print(model)
 
-    # assemble the prompt
-    prompt_to_send = prompt.format(context=system,question=message,history=history_str)
-    print(prompt_to_send)
+    if "claude" in model:
+        # if it's a Claude model, use the Message API - there's no reason to use the completion API in this case
+        messages = claude_message_api_formatter(history)
+        # messages.append({"role" : "user","content" : message})
+        messages.append(HumanMessage(content=message))
+        prompt_to_send = messages
+    else:
+        # assemble the chat history
+        history_str = ""
+        for msg in history:
+            history_str += f'[INST]\n{msg["user"]} [/INST]\n' 
+            history_str += f'{msg["chatbot"]}\n'
+        # assemble the prompt
+        prompt_to_send = prompt.format(context=system,question=message,history=history_str)
+        print(prompt_to_send)
 
     # return a streamed response
     async def stream_response():
         nonlocal prompt_to_send
+        # a bit of a hack - prompt_to_send could either be a list of MessageAPI messages or a simple string
+        # and hopefully the earlier code prevents any mismatches
         async for chunk in llm.astream(prompt_to_send):
             # print(chunk.__anext__())
             yield chunk #.__anext__()
